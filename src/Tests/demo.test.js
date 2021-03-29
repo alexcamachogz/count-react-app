83c2aee085a5da17f075210b78a816dfe4dc21c0
Alejandra Camacho <camacho.ale94@gmail.com>
2021-03-28 21:43:08 -0600

    New testing suites

---
+++ src/Tests/demo.test.js
@@ -0,0 +1,10 @@
+describe('Pruebas en el archivo demo.test.js', () => {
+  test('Strings should be equal', () => {
+    // 1. Inicialización
+    const mensaje = 'Hola Mundo'
+    // 2. Estímulo
+    const mensaje2 = `Hola Mundo`
+    // 3. Observar el comportamiento
+    expect(mensaje).toBe(mensaje2)
+  })
+})
